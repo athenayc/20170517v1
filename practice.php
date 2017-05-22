d18da44d3e83b42def9294d8027ba50b008de5fc
athenatc <athenayc@gmail.com>
2017-05-22 17:35:31 +0800

    20170517 version1

---
+++ practice.php
@@ -0,0 +1,32 @@
+﻿
+<?php
+if (isset($_GET['xx'])) {
+    $x = $_GET['xx'];
+    $y = $_GET['yy'];
+    $a =$x+$y;
+    $b =$x-$y;
+    $c =$x*$y;
+    $d = floor($x/$y);
+
+}
+?>
+
+<form>
+    <input type="text" name="xx" id="x" value="<?php echo $x; ?>"/>
+
+    <select name="name">
+
+        <option >+<?php $r=$x+$y;?></option>
+        <option >+<?php $r=$x+$y;?></option>
+        <option >+<?php $r=$x+$y;?></option>
+        <option >+<?php $r=$x+$y;?></option>
+
+    </select>
+
+    <input type="text" name="yy" id="y" value="<?php echo $y; ?>"/>
+    <input type="submit" value="="/>
+    <?php
+    echo $r;
+    ?>
+</form>
+
